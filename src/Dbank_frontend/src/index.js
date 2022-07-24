71fdcd1dc225332b87e8db148ef42df6fda9b689
yasirrifai <yasirrifai30@gmail.com>
2022-07-24 23:28:13 +0530

    Initial commit.

---
+++ src/Dbank_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { Dbank_backend } from "../../declarations/Dbank_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Dbank_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
